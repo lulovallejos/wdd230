a4e555fde8a6d8c61c38bb139577852283544070
lulovallejos <val20021@byui.edu>
2023-04-20 23:54:37 -0300

    .

---
+++ scripts/date.js
@@ -0,0 +1,41 @@
+let daynames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let d = new Date();
+  let dayName = daynames[d.getDay()];
+  let monthName = months[d.getMonth()];
+  let fulldate =
+    dayName +
+    ", " +
+    monthName +
+    " " +
+    d.getDate() +
+    ", " +
+    d.getFullYear();
+
+  document.getElementById("currentdate").textContent = fulldate;
+
+  document.getElementById("currentyear").innerHTML = new Date().getFullYear();
+
+  let oLastModif = new Date(document.lastModified);
+  document.getElementById("lastUpdated").textContent = oLastModif;
