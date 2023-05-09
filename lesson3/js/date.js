5ec9d3b38ec9c79a02edee86d13853bad5677a61
lulovallejos <val20021@byui.edu>
2023-05-09 00:13:13 -0300

    w3

--- lesson3/js/date.js
+++ lesson3/js/date.js
@@ -1,7 +1,7 @@
 const today = new Date();
 const year = today.getFullYear();
-const msg = `&copy;  ${year}  | Lucas Vallejos | Buenos Aires, Argentina<br/>
-<b>Last Updated: </b>${document.lastModified}`;
+const msg = `&copy; <b> ${year}  | Lucas Vallejos | Buenos Aires, Argentina </b><br/> 
+Last Updated: ${document.lastModified}`;
 
 const el = document.querySelector("footer");
 el.innerHTML = msg;
