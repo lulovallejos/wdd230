f9786bc839d60028d074d12891788ec2812b0878
lulovallejos <val20021@byui.edu>
2023-05-26 10:36:04 -0300

    bom

--- lesson5/scripts/add.js
+++ lesson5/scripts/add.js
@@ -1,38 +1,23 @@
+const list = document.getElementById('list');
+const buttom = document.getElementById('add');
+const input = document.getElementById('favchap');
 
-const input = document.getElementById("favchap");
-const button = document.getElementById("button");
-const list = document.getElementById("list");
-
-
-button.addEventListener("click", () => {
-  if (input.value != null && input.value != "") addFavoriteChapter(input.value);
-  else alert("Please, insert a chapter!");
-});
-
-input.addEventListener("keyup", (event) => {
-  
-  if (input.value != null && input.value != "" && event.key === "Enter")
-    addFavoriteChapter(input.value);
-});
-
-
-const addFavoriteChapter = (favoriteChapter) => {
- 
-  let li = document.createElement("li");
-  
-  let delBtn = document.createElement("button");
- 
-  li.textContent = favoriteChapter;
- 
-  delBtn.textContent = "❌";
- 
-  li.appendChild(delBtn);
-  
-  list.appendChild(li);
-  
-  delBtn.addEventListener("click", () => list.removeChild(li));
-  
-  input.focus();
-  
-  input.value = "";
-};
+buttom.addEventListener("click", () =>
+{
+    if (input.value != '')
+    {
+        li = document.createElement("li");
+        deleteBtn = document.createElement('add');
+        li.innerText = input.value;
+        deleteBtn.innerText = '❌';
+        li.appendChild(deleteBtn);
+        list.appendChild(li);
+        deleteBtn.addEventListener ("click", (event) =>
+        {
+            event.currentTarget.parentNode.remove();
+            input.focus();
+        });
+        input.focus();
+        input.value = '';
+    }
+})
